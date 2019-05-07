6dd11c6d7ee592c91f6514376e45c6debb639600
Diego Sandoval <dsandoval004@west-mec.org>
2019-05-07 13:54:32 -0700

    Store Items Population
    Got the store items to populate based on the array stored

--- JavaScript/store.js
+++ JavaScript/store.js
@@ -4,14 +4,25 @@ window.addEventListener('load', function () {
 // DFUNC: The loadContent function
 function loadContent() {
     // DVARO: 
-    var storeItemsHTML = document.createElement('div');
     // DLOOP: Loops through the storeItems
     for (var i = 0; i < storeItems.length; i++) {
         // DVARO: 
         var itemHTML = document.createElement('div'),
             nameHTML = document.createElement('h3'),
             imageHTML = document.createElement('img'),
-            priceHTML = document.createElement('input'),
+            purchaseButtonHTML = document.createElement('button'),
             descriptionHTML = document.createElement('p');
+        nameHTML.textContent = storeItems[i][0]
+        imageHTML.src = `Images/store/${storeItems[i][0]}/${storeItems[i][1][0]}`
+        purchaseButtonHTML.addEventListener('click', function () {
+            console.log('hi')
+        });
+        purchaseButtonHTML.textContent = 'Add to cart'
+        descriptionHTML.textContent = storeItems[i][3]
+        itemHTML.appendChild(nameHTML)
+        itemHTML.appendChild(imageHTML)
+        itemHTML.appendChild(purchaseButtonHTML)
+        itemHTML.appendChild(descriptionHTML)
+        document.getElementById('storeItems').appendChild(itemHTML)
     };
 };
