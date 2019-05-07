90c029cec96f53e3c2bf29fad455420be414a606
Diego Sandoval <dsandoval004@west-mec.org>
2019-05-07 15:21:48 -0700

    Worked on items more
    Added a qty button to change amount, and now working on engraving

--- JavaScript/store.js
+++ JavaScript/store.js
@@ -10,19 +10,33 @@ function loadContent() {
         var itemHTML = document.createElement('div'),
             nameHTML = document.createElement('h3'),
             imageHTML = document.createElement('img'),
+            qtyHTML = document.createElement('input'),
+            engravingHTML = document.createElement('input'),
             purchaseButtonHTML = document.createElement('button'),
             descriptionHTML = document.createElement('p');
-        nameHTML.textContent = storeItems[i][0]
-        imageHTML.src = `Images/store/${storeItems[i][0]}/${storeItems[i][1][0]}`
-        purchaseButtonHTML.addEventListener('click', function () {
+        // DDOES:
+        nameHTML.textContent = storeItems[i][0];
+        imageHTML.src = `Images/store/${storeItems[i][0]}/${storeItems[i][1][0]}`;
+        // DDOES:
+        qtyHTML.type = 'number';
+        qtyHTML.name = 'qty';
+        qtyHTML.id = 'itemQty';
+        qtyHTML.min = 0;
+        qtyHTML.value = 0;
+        // DDOES:
+        engravingHTML.
+        // DDOES:
+        purchaseButtonHTML.addEventListener('click', function (e) {
             console.log('hi')
         });
-        purchaseButtonHTML.textContent = 'Add to cart'
-        descriptionHTML.textContent = storeItems[i][3]
-        itemHTML.appendChild(nameHTML)
-        itemHTML.appendChild(imageHTML)
-        itemHTML.appendChild(purchaseButtonHTML)
-        itemHTML.appendChild(descriptionHTML)
-        document.getElementById('storeItems').appendChild(itemHTML)
+        purchaseButtonHTML.textContent = 'Add to cart';
+        descriptionHTML.textContent = storeItems[i][3];
+        // DDOES:
+        itemHTML.appendChild(nameHTML);
+        itemHTML.appendChild(imageHTML);
+        itemHTML.appendChild(qtyHTML);
+        itemHTML.appendChild(purchaseButtonHTML);
+        itemHTML.appendChild(descriptionHTML);
+        document.getElementById('storeItems').appendChild(itemHTML);
     };
 };
