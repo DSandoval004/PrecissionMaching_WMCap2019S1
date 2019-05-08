22b7144015d0311e75d262348a6d855021a96cdc
Diego Sandoval <dsandoval004@west-mec.org>
2019-05-08 14:24:46 -0700

    Shop
    made the shop, and added a submit button which says check out on it.

--- JavaScript/store.js
+++ JavaScript/store.js
@@ -3,7 +3,6 @@ window.addEventListener('load', function () {
 });
 // DFUNC: The loadContent function
 function loadContent() {
-    // DVARO: 
     // DLOOP: Loops through the storeItems
     for (var i = 0; i < storeItems.length; i++) {
         // DVARO: 
@@ -12,31 +11,44 @@ function loadContent() {
             imageHTML = document.createElement('img'),
             qtyHTML = document.createElement('input'),
             engravingHTML = document.createElement('input'),
-            purchaseButtonHTML = document.createElement('button'),
+            purchaseButtonHTML = document.createElement('a'),
             descriptionHTML = document.createElement('p');
         // DDOES:
         nameHTML.textContent = storeItems[i][0];
         imageHTML.src = `Images/store/${storeItems[i][0]}/${storeItems[i][1][0]}`;
         // DDOES:
-        qtyHTML.type = 'number';
-        qtyHTML.name = 'qty';
-        qtyHTML.id = 'itemQty';
+        qtyHTML.type = "number";
+        qtyHTML.name = `${storeItems[i][4]}qty`;
+        qtyHTML.id = "itemQty";
         qtyHTML.min = 0;
         qtyHTML.value = 0;
         // DDOES:
-        engravingHTML.
+        engravingHTML.type = "text";
+        engravingHTML.name = `${storeItems[i][4]}eng`;
+        engravingHTML.id = "itemEngraving";
+        engravingHTML.maxLength = 10;
+        engravingHTML.placeholder = "Engraving"
         // DDOES:
         purchaseButtonHTML.addEventListener('click', function (e) {
             console.log('hi')
         });
-        purchaseButtonHTML.textContent = 'Add to cart';
+        purchaseButtonHTML.innerHTML = "Add to cart";
+        purchaseButtonHTML.className = "button";
         descriptionHTML.textContent = storeItems[i][3];
         // DDOES:
         itemHTML.appendChild(nameHTML);
         itemHTML.appendChild(imageHTML);
         itemHTML.appendChild(qtyHTML);
+        itemHTML.appendChild(engravingHTML);
         itemHTML.appendChild(purchaseButtonHTML);
         itemHTML.appendChild(descriptionHTML);
         document.getElementById('storeItems').appendChild(itemHTML);
     };
+    // DVARO: description
+    var submitButton = document.createElement('input');
+    submitButton.id = "submitButton";
+    submitButton.type = "submit";
+    submitButton.value = "Checkout";
+    submitButton.classList = "button";
+    document.getElementById('storeItems').appendChild(submitButton);
 };
